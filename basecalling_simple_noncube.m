4f48c58dd9c8401c84f75e6757d67b9bd969cb84
Daniel Goodwin <dgoodwin208@gmail.com>
2018-05-03 19:01:19 -0400

    new puncta processing and basecalling, leaving behind the old method of generating volumes around the centroid and instead just using the pixels directly

---
+++ basecalling_simple_noncube.m
@@ -0,0 +1,209 @@
+
+
+load(fullfile(params.punctaSubvolumeDir,sprintf('%s_puncta_noncubepixels.mat',params.FILE_BASENAME)));
+
+load('groundtruth_dictionary.mat')
+%% Convert all the data into zscores (very cheap base calling)
+
+%Create a new variable, puncta_set, which is the cropped puncta_set_median
+%for only the bases that we want to call. Given the first three bases are
+%magenta, this could screw things up.
+ROUNDS_TO_CALL = [4 5 6 7 8 9 10 11 12 13 14 15 16 18 20];
+% puncta_set = puncta_set_median_znormalized(ROUNDS_TO_CALL,:,:);
+NUMCALLEDROUNDS = length(ROUNDS_TO_CALL);
+
+puncta_set_median_znormalized = zeros(size(puncta_set_median));
+
+max_z = max(floor(puncta_centroids(:,3)));
+for rnd_idx = 1:20
+    
+    means_z = zeros(max_z,4);
+    stds_z = zeros(max_z,4);
+    for z = 1:max_z
+        indices = find(floor(puncta_centroids(:,3))==z);
+        for c = 1:4
+            means_z(z,c) = mean(puncta_set_median(rnd_idx,c,indices));
+            stds_z(z,c) = std(puncta_set_median(rnd_idx,c,indices));
+        end
+    end
+    
+    
+    for z = 1:max_z
+        indices = find(floor(puncta_centroids(:,3))==z);
+        for c = 1:4
+            p_nonnorm = squeeze(puncta_set_median(rnd_idx,c,indices));
+            p_norm = (p_nonnorm-means_z(z,c))/stds_z(z,c);
+            puncta_set_median_znormalized(rnd_idx,c,indices)=p_norm;
+        end
+    end
+   
+    fprintf('Z-normalized Round%i\n',rnd_idx);
+end
+
+%% Compare the top 10 brightest (zscore) pixels in each puncta across channels
+
+num_puncta = size(puncta_set,3);
+
+%Pre-initialize the cell arrray and determine the basecalls
+
+base_calls_quickzscore = zeros(num_puncta,params.NUM_ROUNDS);
+base_calls_quickzscore_confidence = zeros(num_puncta,params.NUM_ROUNDS);
+base_calls_pixel_intensity = zeros(num_puncta,params.NUM_ROUNDS,params.NUM_CHANNELS);
+base_calls_zscores = zeros(num_puncta,params.NUM_ROUNDS,params.NUM_CHANNELS);
+
+puncta_set_roundnormed = zeros(size(puncta_set_median_znormalized));
+
+for rnd_idx = 1:NUMCALLEDROUNDS
+    
+    actual_rnd_idx = ROUNDS_TO_CALL(rnd_idx);
+    clear chan_col; %Just in case, otherwise the for loop can error.
+    for c = params.COLOR_VEC
+        chan_col(:,c) = reshape(puncta_set_median_znormalized(actual_rnd_idx,c,:),[],1);
+    end
+    
+    % cols_normed = quantilenorm(chan_col);
+    cols_normed = zscore(chan_col);
+    
+    for c = params.COLOR_VEC
+        puncta_set_roundnormed(actual_rnd_idx,c,:) = reshape(cols_normed(:,c),size(squeeze(puncta_set_median_znormalized(actual_rnd_idx,c,:))));
+    end
+    
+    fprintf('Looping through puncta from round %i\n',rnd_idx);
+    for p_idx= 1:num_puncta
+        
+        
+        scores = squeeze(puncta_set_roundnormed(actual_rnd_idx,:,p_idx));
+        
+        %add the minimum score to shift everything non-zero
+        scores = scores - min(scores);
+        
+        %and the new baseguess
+        [~, newbaseguess] = max(scores);
+        base_calls_quickzscore(p_idx,actual_rnd_idx) = newbaseguess;
+        
+        [scores_sorted,~] = sort(scores,'descend');
+        base_calls_quickzscore_confidence(p_idx,actual_rnd_idx) = scores_sorted(1)/(scores_sorted(1)+ scores_sorted(2));
+        
+        %use the baseguess to get the absolute brightness of the puncta
+        base_calls_pixel_intensity(p_idx,actual_rnd_idx) = puncta_set_median(rnd_idx,newbaseguess,p_idx);
+        
+        base_calls_zscores(p_idx,actual_rnd_idx,:) = scores;
+    
+    end
+    
+end
+
+[unique_transcipts,~,~] = unique(base_calls_quickzscore,'rows');
+fprintf('Found %i transcripts, %i of which are unique\n',size(base_calls_quickzscore,1),size(unique_transcipts,1));
+
+%% Do a quick visualization of the base calls across rounds
+for base_idx = 1:params.NUM_CHANNELS
+    perc_base(:,base_idx) = sum(base_calls_quickzscore==base_idx,1)/size(base_calls_quickzscore,1);
+end
+figure;
+% Chan 1 = Blue
+% Chan 2 = Green
+% Chan 3 = Magenta
+% Chan 4 = Red
+
+plot(perc_base(:,1)*100,'b','LineWidth',2); hold on;
+plot(perc_base(:,2)*100,'g','LineWidth',2)
+plot(perc_base(:,3)*100,'m','LineWidth',2)
+plot(perc_base(:,4)*100,'r','LineWidth',2); hold off;
+legend('Chan1 - FITC','Chan2 - CY3', 'Chan3 - Texas Red', 'Chan4 - Cy5');
+title(sprintf('Percentage of each base across rounds for %i puncta',size(transcripts,1)));
+
+
+%% Make sets of transcripts and create a new transcript object
+if ~exist('gtlabels','var')
+    load('groundtruth_dictionary_slice.mat');
+end
+%
+gt_mask = ROUNDS_TO_CALL-3;
+transcript_objects = cell(size(base_calls_quickzscore,1),1);
+
+output_cell = {}; ctr = 1;
+discarded_puncta = []; discard_ctr = 1;
+for p_idx = 1:size(base_calls_quickzscore,1)
+    
+    transcript = struct;
+    %Search for a perfect match in the ground truth codes
+    img_transcript = base_calls_quickzscore(p_idx,ROUNDS_TO_CALL);
+    
+    %Sanity check: randomize the img_transcript
+    %     img_transcript = img_transcript(randperm(length(img_transcript)));
+    
+    %Search for a perfect match in the ground truth codes
+    hits = (groundtruth_codes(:,gt_mask)==img_transcript);
+    
+    %Calculate the hamming distance (now subtracting primer length)
+    scores = length(img_transcript)- sum(hits,2);
+    [values, indices] = sort(scores,'ascend');
+    
+    best_score = values(1);
+    %Get the first index that is great than the best score
+    %If this is 1, then there is a unique fit
+    %     idx_last_tie = find(values>best_score,1)-1;
+    idx_last_tie = 1;
+    while values(idx_last_tie)<=best_score %Trying a faster method than find()
+        idx_last_tie = idx_last_tie +1;
+    end
+    idx_last_tie = idx_last_tie-1;
+    
+    intensities = squeeze(base_calls_pixel_intensity(p_idx,:,:));
+
+    %Assuming the groundtruth options are de-duplicated
+    %Is there a perfect match to the (unique ) best-fit
+    transcript.img_transcript=base_calls_quickzscore(p_idx,:);
+    transcript.img_transcript_confidence=base_calls_quickzscore_confidence(p_idx,:);
+    transcript.img_transcript_absValuePixel=intensities;
+    transcript.img_transcript_zscores=squeeze(base_calls_zscores(p_idx,:,:));
+    transcript.pos = puncta_centroids(p_idx,:);
+    transcript.hamming_score = best_score;
+    transcript.numMatches = idx_last_tie;
+    
+%     transcript.nn_distance = spacings(p_idx);
+    
+    %If the alignment to known sequences is one error or less
+    if best_score <=1
+        row_string = sprintf('%i,%s,',p_idx,mat2str(img_transcript));
+        
+        %If it's a unique match, use the name
+        if idx_last_tie==1
+            transcript.known_sequence_matched = groundtruth_codes(indices(idx_last_tie),:);
+            transcript.name = gtlabels{indices(idx_last_tie)};
+        end
+        
+        for tiedindex = 1:idx_last_tie
+            row_string = strcat(row_string,sprintf('%s,',gtlabels{indices(tiedindex)}));
+        end
+        row_string = sprintf('%s\n',row_string);
+        fprintf('%s',row_string);
+        output_cell{ctr} = row_string; ctr = ctr+1;
+    end
+    
+    transcript_objects{p_idx} = transcript;
+    
+    clear transcript; %Avoid any accidental overwriting
+    
+    if mod(p_idx,1000) ==0
+        fprintf('%i/%i matched\n',p_idx,size(puncta_set,6));
+    end
+end
+fprintf('Done!\n');
+
+output_csv = strjoin(output_cell,'');
+
+output_file = fullfile(params.transcriptResultsDir,sprintf('%s_simpleextractedcodes.csv',params.FILE_BASENAME));
+
+fileID = fopen(output_file,'w');
+fprintf(fileID,output_csv);
+fclose(fileID);
+
+%%
+%remove the transcripts that had an empty round, likely because of the registration
+transcript_objects(discarded_puncta) = [];
+
+save(fullfile(params.transcriptResultsDir,sprintf('%s_transcriptmatches_objects.mat',params.FILE_BASENAME)),'transcript_objects','-v7.3');
+fprintf('Saved transcript_matches_objects!\n');
+
---
+++ puncta_roicollect_simple.m
@@ -0,0 +1,91 @@
+loadParameters;
+
+filename_centroids = fullfile(params.punctaSubvolumeDir,sprintf('%s_centroids+pixels.mat',params.FILE_BASENAME));
+load(filename_centroids,'puncta_centroids','puncta_voxels')
+%% Collect the subvolumes we started this with, but now only with the pixels from the puncta!
+
+num_insitu_transcripts = size(puncta_voxels,1);
+
+%Define a puncta_set object that can be parallelized
+puncta_set_cell = cell(params.NUM_ROUNDS,1);
+
+
+%the puncta indices are here in linear form for a specific round
+
+parpool(5); %arbitrary but this parallel loop is memory intensive
+parfor exp_idx = 1:params.NUM_ROUNDS
+    disp(['round=',num2str(exp_idx)])
+    pixels_per_rnd = cell(num_insitu_transcripts,params.NUM_CHANNELS);
+    
+    for c_idx = params.COLOR_VEC
+        filename_in = fullfile(params.registeredImagesDir,sprintf('%s_round%.03i_%s_%s.tif',params.FILE_BASENAME,exp_idx,params.CHAN_STRS{c_idx},regparams.REGISTRATION_TYPE));
+        img =  load3DTif_uint16(filename_in);
+        
+        for puncta_idx = 1:num_insitu_transcripts
+            
+            indices_for_puncta = puncta_voxels{puncta_idx};
+            
+            %This makes a volume that is all zeros except for the punctafeinder_indices_for_puncta
+            pixels_for_puncta_set = img(indices_for_puncta);
+            
+            %Then we take the PUNCTA_SIZE region around those pixels only
+            pixels_per_rnd{puncta_idx,c_idx} = pixels_for_puncta_set;
+            
+            if mod(puncta_idx,5000)==0
+                fprintf('Rnd %i, Chan %i, Puncta %i processed\n',exp_idx,c_idx,puncta_idx);
+            end
+            
+        end
+        
+    end
+    puncta_set_cell{exp_idx} = pixels_per_rnd;
+
+end
+
+disp('reducing processed puncta')
+puncta_set_median = zeros(params.NUM_ROUNDS,params.NUM_CHANNELS,num_insitu_transcripts);
+% reduction of parfor
+for puncta_idx = 1:num_insitu_transcripts
+    for exp_idx = 1:params.NUM_ROUNDS
+        for c_idx = params.COLOR_VEC
+            % Each puncta_set_cell per exp is
+            % pixels_per_rnd = cell(num_insitu_transcripts,params.NUM_CHANNELS);
+            puncta_set_median(exp_idx,c_idx,puncta_idx) = median(puncta_set_cell{exp_idx}{puncta_idx,c_idx});
+            %             puncta_set_mean(exp_idx,c_idx,puncta_idx) = mean(puncta_set_cell{exp_idx}{puncta_idx,c_idx});
+            %             puncta_set_max(exp_idx,c_idx,puncta_idx) = max(puncta_set_cell{exp_idx}{puncta_idx,c_idx});
+        end
+    end
+end
+
+% Using median values to ignore bad points
+channels_present = squeeze(sum(puncta_set_median==0,2));
+num_roundspresent_per_puncta = squeeze(sum(channels_present==0,1));
+%Create a mask of all puncta that have a non-zero signal in all four
+%channels for all rounds
+signal_complete = num_roundspresent_per_puncta==params.NUM_ROUNDS;
+
+fprintf('Number of complete puncta: %i \n',sum(signal_complete));
+
+puncta_set_median = puncta_set_median(:,:,signal_complete);
+puncta_centroids = puncta_centroids(signal_complete,:);
+
+puncta_size = zeros(sum(signal_complete),1);
+indices_of_good_puncta = find(signal_complete);
+for puncta_idx = 1:size(puncta_centroids,1)
+    puncta_size(puncta_idx) = length(puncta_voxels{indices_of_good_puncta(puncta_idx)});
+end
+%just save puncta_set
+% outputfile = '/mp/nas0/ExSeq/AutoSeq2/xy10/6_transcripts/exseqauto-xy10_puncta_noncubepixels';
+outputfile = fullfile(params.transcriptResultsDir,sprintf('%s_puncta_noncubepixels.mat',params.FILE_BASENAME));
+save(outputfile,'puncta_set_median','puncta_centroids','puncta_size','-v7.3');
+
+%% Analyze the output
+% Using median values to ignore bad points
+
+% figure;
+% plot3(puncta_centroids(~signal_complete,1),puncta_centroids(~signal_complete,2),...
+%     puncta_centroids(~signal_complete,3),'b.');
+% hold on;
+% plot3(puncta_centroids(signal_complete,1),puncta_centroids(signal_complete,2),...
+%     puncta_centroids(signal_complete,3),'rx','MarkerSize',5);
+
